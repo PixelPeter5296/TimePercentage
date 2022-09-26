f42f626f4d40b3b092de4125e4706c4dcca1a8e2
PixelPeter5296 <85898675+PixelPeter5296@users.noreply.github.com>
2022-09-26 10:08:07 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+setInterval(() => { document.body.innerHTML = "<h1 style='font-family:Helvetica'>" + parseFloat(
+    	(new Date().getHours() * 60 * 60 * 1000 + new Date().getMinutes() * 60 * 1000 + new Date().getSeconds() * 1000 + new Date().getMilliseconds() - (8*60*60*1000)) / (270*60*1000) * 100
+    ).toFixed(4) + "%</h1><p style='font-family:Helvetica'>vom Tag fertig.</p>"
+}, 1)
